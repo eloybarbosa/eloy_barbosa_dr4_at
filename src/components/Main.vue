58e84376bc9888a9b2255b17f129216d68a524c6
eloybarbosa <eloy.barbosa@al.infnet.edu.br>
2020-03-30 15:04:10 -0300

    Adicionando condicional de Saudação na Main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,11 @@
 <template>
 
 <div class="container">
+<h2 class="display-4 textocentro" id="dia" v-if="hora >= 0 && hora < 12"> Bom dia, seja Bem Vindo!!!</h2>
+<h2 class="display-4 textocentro" id="tarde" v-else-if="hora >= 12 && hora < 18"> Boa tarde, seja Bem Vindo!!!</h2>
+<h2 class="display-4 textocentro" id="noite" v-else> Boa noite, seja Bem Vindo!!!</h2>
 <h2 class="display-4 textocentro">Mural do Eloy</h2>
-<h2 class="display-4 textocentro">Seja Bem Vindo!!!</h2>
+
 
 <router-link to="/AddComentario" class="btn btn-dark btn-lg btn-block">Adicionar um comentário</router-link>
 <router-link to="/Comentarios" class="btn btn-dark btn-lg btn-block">Visualizar Mural</router-link>
@@ -14,5 +17,10 @@
 <script>
 export default {
   name: 'Main',
+  data: function() {
+    return {
+      hora: new Date().getHours()
+    }
+  }
 }
 </script>
